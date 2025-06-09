8941935d7a6c98710811ca48889040eb67e4379e
kawanescola <kawan.miguel.marques@escola.pr.gov.br>
2025-06-09 08:37:01 -0300

    sketch.js
    colocando o código

--- sketch.js
+++ sketch.js
@@ -2,7 +2,60 @@ function setup() {
   createCanvas(400, 400);
 }
 
+let xJogador = [0, 0, 0, 0];
+let yJogador = [75, 150, 225, 300];
+let jogador = ["🚜", "🌄", "🏙️", "🚘"];
+let teclas = ["a", "s", "d", "f"];
+let quantidade = jogador.length;
 
 function draw() {
-  background(220);
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
 }
+var img;
+
+function preload(){
+  img = loadImage("background.png");
+}
+
+
+function ativaJogo() {
+    background(img);
+  }
+
+function desenhaJogadores() {
+  textSize(40);
+  for (let i = 0; i < quantidade; i++) {
+    text(jogador[i], xJogador[i], yJogador[i]);
+  }
+}
+
+function desenhaLinhaDeChegada() {
+  fill("white");
+  rect(350, 0, 10, 400);
+  fill("black");
+  for (let yAtual = 0; yAtual < 400; yAtual += 20) {
+    rect(350, yAtual, 10, 10);
+  }
+}
+
+function verificaVencedor() {
+  for (let i = 0; i < quantidade; i++) {
+    if (xJogador[i] > 350) {
+      text(jogador[i] + " venceu!", 50, 200);
+      noLoop();
+    }
+  }
+}
+
+function keyReleased() {
+  for (let i = 0; i < quantidade; i++) {
+    if (key == teclas[i]) {
+      xJogador[i] += random(20);
+    }
+  }
+}
+
+// Quero um background de uma cidade / campo, meio a meio horizontal, para meu jogo de corrida com o tema Festejando a Conexão Campo-Cidade, do agrinho. Quero este background no psj5, pois meu jogo é dentro de lá.
